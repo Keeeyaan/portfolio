f107884145acbe010af0f68c7060acc12803f1a0
keeeyaan <keanjieden@gmail.com>
2024-05-03 21:56:25 +0800

    fixed contact section not responsive

--- src/components/section/ContactSection.tsx
+++ src/components/section/ContactSection.tsx
@@ -7,12 +7,12 @@ const ContactSection = () => {
   return (
     <section id="contact" className="max-w-7xl px-5 w-full mx-auto mb-4 mt-20">
       <h1 className="text-center text-3xl font-bold mb-8">Contact Me</h1>
-      <div className="flex justify-center items-center gap-10">
+      <div className="flex justify-center items-center gap-10 flex-wrap">
         <div className="space-y-6">
-          <h1 className="text-4xl font-semibold leading-normal">
+          <h1 className="text-4xl text-center lg:text-start font-semibold leading-normal">
             Let&apos;s chat. <br /> Tell me about your <br /> project.
           </h1>
-          <p className="dark:text-muted-foreground text-white">
+          <p className="dark:text-muted-foreground text-white text-center lg:text-start ">
             Let&apos;s create something together🤘
           </p>
           <Card>
